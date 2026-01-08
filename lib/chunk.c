891ce207ac792472d921a9d27b3ce37f30f54971
emad lakhbizi <assimad8@gmail.com>
2026-01-08 17:22:26 +0100

    Implement constant values and bytecode representation

--- lib/chunk.c
+++ lib/chunk.c
@@ -8,6 +8,7 @@ void initChunk(Chunk *chunk)
     chunk->capacity=0;
     chunk->count=0;
     chunk->code=NULL;
+    initValueArray(&chunk->constants);
 }
 
 void writeChunk(Chunk *chunk, uint8_t byte)
@@ -21,8 +22,15 @@ void writeChunk(Chunk *chunk, uint8_t byte)
     chunk->code[chunk->count++] = byte;
 }
 
+int addConstant(Chunk *chunk, Value value)
+{
+    writeValueArray(&chunk->constants,value);
+    return chunk->constants.count-1;
+}
+
 void freeChunk(Chunk *chunk)
 {
     FREE_ARRAY(uint8_t,chunk->code,chunk->capacity);
+    freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
