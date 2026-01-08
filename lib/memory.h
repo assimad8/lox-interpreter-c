4900a5782037affa4ae95f5056225e2860a264be
emad lakhbizi <assimad8@gmail.com>
2026-01-08 16:32:17 +0100

    ddisassembling chunks chapter [14.4]

--- lib/memory.h
+++ lib/memory.h
@@ -1,14 +1,14 @@
 #ifndef clox_memory_h
 #define clox_memory_h
 
-#define GROW_ARRAY
 #define GROW_CAPACITY(capacity) \
     ((capacity)<8?8:(capacity)*2)
 #define GROW_ARRAY(type,pointer,oldCount,newCount) \
-    (type*)reallocate(pointer,sizeof(type)*(oldCount),\
-    sizeof(type)*newCount)
+    (type*)reallocate(pointer,sizeof(type)*(oldCount),sizeof(type)*newCount)
 
 #define FREE_ARRAY(type,pointer,capacity) \
     GROW_ARRAY(type,pointer,sizeof(pointer)*capacity,0)
+
+void* reallocate(void *pointer,size_t oldSize,size_t newSize);
 
 #endif //clox_memory_h
--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include "lib/common.h"
+#include "lib/chunk.h"
+#include "lib/debug.h"
 
 int main(int argc, char *argv[]){
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk,OP_RETURN);
+
+    disassembleChunk(&chunk,"Test chunk");
+
+    freeChunk(&chunk);
     return 0;
 }
