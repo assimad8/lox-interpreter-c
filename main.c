64e5a97d1296d7677c323406c9ef99d2cd36c461
emad lakhbizi <assimad8@gmail.com>
2026-01-11 16:01:08 +0100

    feat(vm): implement instruction execution machine

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include "lib/common.h"
+#include "lib/common.h"
 #include "lib/chunk.h"
 #include "lib/debug.h"
+#include "lib/vm.h"
 
 int main(int argc, char *argv[]){
+    // initVM();
     Chunk chunk;
     initChunk(&chunk);
     int constant = addConstant(&chunk,1.2);
@@ -11,7 +14,8 @@ int main(int argc, char *argv[]){
     writeChunk(&chunk,OP_RETURN,123);
 
     disassembleChunk(&chunk,"Test chunk");
-
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
