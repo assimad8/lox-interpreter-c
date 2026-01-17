ca8881bf8502898e50eda865798851095caba4de
emad <assimad8@gmail.com>
2026-01-17 18:48:16 +0100

    feat(variables): add global variable declaration via var statements
        - Introduce var statement parsing for global variable declarations
        - Emit OP_DEFINE_GLOBAL to create new global variables
        - Store variable names as interned strings in the constant pool
        - Initialize declared globals with provided initializer values
        - Default uninitialized globals to nil
        - Insert declared variables into vm.globals hash table
        - Allow subsequent reads and writes to declared globals
        - Enable basic name binding without local scope or resolution

--- lib/vm.c
+++ lib/vm.c
@@ -33,12 +33,14 @@ void initVM()
 {
     resetStack();
     vm.objects = NULL;
+    initTable(&vm.globals);
     initTable(&vm.strings);
 }
 
 void freeVM()
 {
     freeObjects();
+    freeTable(&vm.globals);
     freeTable(&vm.strings);
 }
 
@@ -75,6 +77,7 @@ static InterpretResult run()
 {
     #define READ_BYTE() (*vm.ip++)
     #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+    #define READ_STRING() AS_STRING(READ_CONSTANT())
     #define BINARY_OP(ValueType,op) \
         do {\
             if(!IS_NUMBER(peek(0))||!IS_NUMBER(peek(1))){\
@@ -127,6 +130,12 @@ static InterpretResult run()
                 break;
             }
             case OP_POP: {
+                pop();
+                break;
+            }
+            case OP_DEFINE_GLOBAL: {
+                ObjString* name = READ_STRING();
+                tableSet(&vm.globals,name,peek(0));
                 pop();
                 break;
             }
@@ -203,6 +212,7 @@ static InterpretResult run()
 
     #undef READ_BYTE
     #undef READ_CONSTANT
+    #undef READ_STRING
     #undef BINARY_OP
 
     return INTERPRET_OK;
