6b86fcaf62445fc83916348563f91e1d7cd9912d
emad <assimad8@gmail.com>
2026-01-22 23:46:00 +0100

    feat(logical-operators): implement short-circuiting and/or expressions
        - Parse logical 'and' and 'or' expressions
        - Implement short-circuit evaluation semantics
        - Emit conditional jumps to skip right-hand operands
        - Patch jump offsets after compiling operands
        - Pop intermediate condition values correctly
        - Preserve correct precedence and associativity
        - Integrate logical operators into Pratt parser

--- src/compiler.c
+++ src/compiler.c
@@ -364,6 +364,24 @@ static void unary(bool canAssing)
         default: return;//Unreachable
     }
 }
+static void and_(bool cannAssgin)
+{
+    int endJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+    parsePrecedence(PREC_AND);
+
+    patchJump(endJump);
+}
+static void or_(bool cannAssgin)
+{
+    int elseJump = emitJump(OP_JUMP_IF_FALSE);
+    int endJump = emitJump(OP_JUMP);
+    patchJump(elseJump);
+    emitByte(OP_POP);
+    parsePrecedence(PREC_AND);
+    patchJump(endJump);
+
+}
 
 ParseRule rules[] = {
     [TOKEN_LEFT_PAREN]    = {grouping,NULL,PREC_NONE},[TOKEN_RIGHT_PAREN]   = {NULL,NULL,PREC_NONE},
@@ -387,7 +405,7 @@ ParseRule rules[] = {
     [TOKEN_IDENTIFIER]    = {variable,NULL,PREC_NONE},
     [TOKEN_STRING]        = {string,NULL,PREC_NONE},
     [TOKEN_NUMBER]        = {number,NULL,PREC_NONE},
-    [TOKEN_AND]           = {NULL,NULL,PREC_NONE},
+    [TOKEN_AND]           = {NULL,and_,PREC_AND},
     [TOKEN_CLASS]         = {NULL,NULL,PREC_NONE},
     [TOKEN_ELSE]          = {NULL,NULL,PREC_NONE},
     [TOKEN_FALSE]         = {literal,NULL,PREC_NONE},
@@ -395,7 +413,7 @@ ParseRule rules[] = {
     [TOKEN_FUN]           = {NULL,NULL,PREC_NONE},
     [TOKEN_IF]            = {NULL,NULL,PREC_NONE},
     [TOKEN_NIL]           = {literal,NULL,PREC_NONE},
-    [TOKEN_OR]            = {NULL,NULL,PREC_NONE},
+    [TOKEN_OR]            = {NULL,or_,PREC_OR},
     [TOKEN_PRINT]         = {NULL,NULL,PREC_NONE},
     [TOKEN_RETURN]        = {NULL,NULL,PREC_NONE},
     [TOKEN_SUPER]         = {NULL,NULL,PREC_NONE},
